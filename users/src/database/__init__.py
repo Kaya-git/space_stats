1d0a0b4e30ddeef091704c0f709569e053638fe1
Evgeny Buzuev <ezjell@yandex.ru>
2024-02-22 17:27:58 +0300

    allembic users

--- users/src/database/__init__.py
+++ users/src/database/__init__.py
@@ -1,5 +1,5 @@
 from .db import Database, get_async_session
 from .base_model import Base
-from .models import User, Role
+from .models import User, Role, ApiKey
 
-__all__ = ["Database", "Base", "User", "Role", "get_async_session"]
+__all__ = ["Database", "Base", "User", "Role", "get_async_session", "ApiKey"]
