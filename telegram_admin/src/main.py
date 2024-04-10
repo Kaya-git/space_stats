1ee5be78a718bc4c3d537389f9f833a7570a1f67
Evgeny Buzuev <ezjell@yandex.ru>
2024-04-10 20:50:17 +0300

    new service telegram admin

--- telegram_admin/src/main.py
+++ telegram_admin/src/main.py
@@ -2,10 +2,12 @@ from aiogram import Bot, Dispatcher, types
 from config import conf
 from aiogram.filters import CommandStart
 from keyboards.reply import kb_main_menu
-
+from logic.set_tg_group_tasks import set_tg_tasks
 
 bot = Bot(token=conf.telegram.bot_token)
 dp = Dispatcher()
+
+dp.include_router(set_tg_tasks)
 
 
 @dp.message(CommandStart())
